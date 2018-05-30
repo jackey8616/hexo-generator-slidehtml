19efb3562fa70d76f42a17ca23b77edad0d0996c
clode <jackey8616@gmail.com>
2018-05-30 15:02:09 +0800

    Fix sync problem
    Fixed the issue cause by sync.
     - create dir withou sync cause writeFile trigger too early.
    Add package.json and publish onto npm.
    Add hexo config in order to automatically detect layout folder.
    
    Next commit would try to seek methods to inject layout-slide.ejs auto-
    matically.

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-/** 
- * Created by clo5de on 2018/05/30.
+/**
+ * Created by clooooode on 2018/05/30.
  */
 
+
+var hexo = hexo != undefined ? hexo : {};
+var hexoConfig = hexo.config
+
 var fs = require('fs');
+//var fs = require('hexo-fs');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var ejs = require('ejs');
 var yfm = require('hexo-front-matter');
 var sourceRoot = path.join('source', '_posts');
-var templatePath= path.join('themes', 'landscape', 'layout', 'post-slide.ejs');
+var templatePath= path.join('themes', hexoConfig.theme, 'layout', 'post-slide.ejs');
 var outPathRoot = 'public';
 
-var hexo = hexo != undefined ? hexo : {}; 
+var files = fs.readdirSync(path.join(sourceRoot));
+
 
 hexo.on("generateBefore", function(){
     console.log("Generating slide html file. ", __dirname)
@@ -21,21 +27,21 @@ hexo.on("generateBefore", function(){
         if(fileName.substring(fileName .lastIndexOf("."))=='.md') {
 
             // Reading Markdown
-            var page = fs.readFileSync(path.join(sourceRoot, _item.toString()), 'UTF-8');
+            var fileName = _item.toString();
+            var page = fs.readFileSync(path.join(sourceRoot, fileName), 'UTF-8');
 
             // Reading title and date
             var title = yfm.parse(page).title;
-            var date = new Date(yfm.parse(page).date);
-            page = page.replace(/([\r\n\0]*?)---([\S\s]*?)---([\r\n\0]*?)/g, title+'\n');
+            var date = yfm.parse(page).date != undefined ? new Date(yfm.parse(page).date) : new Date();
+            page = page.replace(/([\r\n\0]*?)---([\S\s]*?)---([\r\n\0]*?)/g, title + '\n');
 
             // Concat output path
             var year = date.getFullYear().toString();
             var month = ("0" + (date.getMonth() + 1)).slice(-2).toString();
             var day = ("0" + date.getDate()).slice(-2).toString();
-            //var outPath = path.join(outPathRoot, year, month, day, title);
-            var outPath = path.join(outPathRoot, 'slides');
+            var outPath = path.join(outPathRoot, year, month, day, fileName.replace('.md', ''));
             if (!fs.existsSync(outPath)) {
-                mkdirp(outPath, function (err) {
+                mkdirp.sync(outPath, function (err) {
                     if (err) {
                         console.log(err);
                         //throw err;
@@ -46,17 +52,17 @@ hexo.on("generateBefore", function(){
             // Output to HTML file.
             var template = fs.readFileSync(templatePath, 'utf-8');
             var pageData = ejs.render(template, {
-                filename:title,
+                filename: title,
                 page: page
             });
-            fs.writeFile(path.join(outPath, _item.toString().replace('.md', '.html')), pageData, function (err) {
+            fs.writeFileSync(path.join(outPath, 'index-slide.html'), pageData, function (err) {
                 if (err) {
                     console.log(err);
-                    //throw err;
+                    // throw err;
                 }
             });
         }
-        console.log(path.join(outPath, _item.toString()) + ' slide generated.');
+        console.log(outPath + ' slide generated.');
     });
     console.log('Slides all generated.');
 });
